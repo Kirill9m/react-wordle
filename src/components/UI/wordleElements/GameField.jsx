3aa227c10b72317260e0101667bb73104a285600
Kirill <kirillsavorin@gmail.com>
2025-03-30 13:54:18 +0200

    main: add gamefield object

--- src/components/UI/wordleElements/GameField.jsx
+++ src/components/UI/wordleElements/GameField.jsx
@@ -1,7 +1,45 @@
 import classes from './GameField.module.css';
+import { useState } from 'react';
 
 export const GameField = () => {
-  return(
-    <h1>test</h1>
+  const table = [
+    { row: 1, col: 1 },
+    { row: 2, col: 1 },
+    { row: 3, col: 1 },
+    { row: 4, col: 1 },
+    { row: 5, col: 1 },
+    { row: 1, col: 2 },
+    { row: 2, col: 2 },
+    { row: 3, col: 2 },
+    { row: 4, col: 2 },
+    { row: 5, col: 2 },
+    { row: 1, col: 3 },
+    { row: 2, col: 3 },
+    { row: 3, col: 3 },
+    { row: 4, col: 3 },
+    { row: 5, col: 3 },
+    { row: 1, col: 4 },
+    { row: 2, col: 4 },
+    { row: 3, col: 4 },
+    { row: 4, col: 4 },
+    { row: 5, col: 4 },
+    { row: 1, col: 5 },
+    { row: 2, col: 5 },
+    { row: 3, col: 5 },
+    { row: 4, col: 5 },
+    { row: 5, col: 5 },
+    { row: 1, col: 6 },
+    { row: 2, col: 6 },
+    { row: 3, col: 6 },
+    { row: 4, col: 6 },
+    { row: 5, col: 6 },
+  ];
+
+  const [tableData, setTableData] = useState(table);
+
+  console.log(tableData);
+
+  return (
+    <h1>1</h1>
   )
-}
+};

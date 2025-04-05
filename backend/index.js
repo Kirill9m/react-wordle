3b06d12c0515beaee2c037d1eab5ecd51268d95a
Kirill <kirillsavorin@gmail.com>
2025-04-06 00:20:24 +0200

    feat: backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import fs from 'fs/promises'
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.get('/', async(req, res) => {
+  const buf = await fs.readFile('../frontend/dist/index.html')
+  const html = buf.toString();
+  res.send(html);
+});
+
+app.use('/assets', express.static('../frontend/dist/assets'));
+
+app.listen(5080, () => {
+  console.log(`Server running on 5080`);
+});

3dc5d690fcd5b1bddcfdffb9e1a4c1d88d86eda4
Kirill <kirillsavorin@gmail.com>
2025-04-06 10:11:38 +0200

    imp: server logic

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -2,29 +2,34 @@ import express from "express";
 import { chooseWord } from "../logic/chooseWord.js";
 import { checkWord } from "../logic/checkWord.js";
 import { wordList } from "../logic/words.js";
+import { currentTime } from "../logic/currentTime.js";
 
 const router = express.Router();
 const games = []; //MongoDB
 
 router.get("/start", (req, res) => {
-
   const playerId = req.body.playerId; //Req from DB in future
   const length = parseInt(req.body.length);
-  const unique = req.body.unique === 'true';
+  const unique = req.body.unique === "true";
+  const time = currentTime();
 
   if (!playerId) {
     return res.status(400).json({ error: "Player ID is required." });
   }
 
   try {
-    games[playerId] = { word: chooseWord(wordList, length, unique)};
-    console.log(games[playerId]);
+    games[playerId] = {
+      id:  playerId,
+      word: chooseWord(wordList, length, unique),
+      gameStarted: time,
+      isUnique: unique,
+    };
+    console.log( games[playerId] );
     res.json({ status: `Game for playerId ${playerId} is started`, length });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-
 
 router.post("/guess", (req, res) => {
   const playerId = req.body.playerId;
@@ -35,9 +40,17 @@ router.post("/guess", (req, res) => {
   }
 
   const currentGame = games[playerId];
+  const timeNow = currentTime();
+  const timeStarted = currentGame.gameStarted;
+  const gameUnique = currentGame.isUnique;
   const result = checkWord(guess, currentGame.word, playerId);
 
-  res.json({ result, guess, });
+  if (result === true) {
+    delete games[playerId];
+    console.log(`Game for player: ${playerId} ended ${timeNow} and was removed.`);
+  }
+
+  res.json({ result, guess, timeNow, timeStarted, gameUnique });
 });
 
 export default router;

c196f43c4ce7b992125aa7412cab90debb829d26
Kirill <kirillsavorin@gmail.com>
2025-04-06 12:38:05 +0200

    feat: unique checkbox

--- frontend/src/components/WordleGame.jsx
+++ frontend/src/components/WordleGame.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 
 const WordleGame = () => {
   const [input, setInput] = useState('');
+  const [id, setId] = useState('');
+  const [unique, setUnique] = useState(false);
 
   const handleInputWord = (e) => {
-    setInput((c) => e.target.value);
+    setInput(() => e.target.value);
+  };
+
+  const handleUniqueStatus = (e) => {
+    setUnique(() => e.target.value);
   };
 
   const sendWord = async () => {
@@ -24,6 +30,26 @@ const WordleGame = () => {
 
   return (
     <div className="game__container">
+      <label>
+        <input
+          type="radio"
+          value="true"
+          checked={unique === 'true'}
+          onChange={handleUniqueStatus}
+        />
+        Unique
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="false"
+          checked={unique === 'false'}
+          onChange={handleUniqueStatus}
+        />
+        Not Unique
+      </label>
+
+
       <p className="game__container__word">The guessed word is: {input}</p>
       <input
         className="game__container__input"

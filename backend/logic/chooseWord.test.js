04b930b1658aa9b5d4567cc43640208f873d8f08
Kirill <kirillsavorin@gmail.com>
2025-04-06 08:49:46 +0200

    fix: split tests into separate files

--- backend/logic/chooseWord.test.js
+++ backend/logic/chooseWord.test.js
@@ -1,14 +1,6 @@
-import { checkWord } from "./checkWord";
 import { chooseWord } from "./chooseWord";
 import { describe, expect, it } from "@jest/globals";
 /*
-  The plan is(TDD word check):
-    Check if the words match(lowercase) & return true
-    if not
-      Verify if we receive an array that splits into characters
-      Verify that the array has characters as objects with correct properties
-    Improvements:
-      Check if the inputs are valid strings of the same length
   Next step(TDD Random word):
     Check if the function returns a word from the list
     Check if the word is randomly picked
@@ -18,38 +10,6 @@ import { describe, expect, it } from "@jest/globals";
     Improvements:
       Function can handle an empty word list 
 */
-
-describe("checkWord()", () => {
-  it("should return true if words are same and lower case", () => {
-    const output = checkWord("Worms", "worms");
-    expect(output).toEqual(true);
-  });
-
-  it("if the words are not the same should return any array", () => {
-    const output = checkWord("Worms", "Wormi");
-    expect(output).toEqual(expect.any(Array));
-  });
-
-  it("if the words are not the same, should split the word into characters and store it in an object as letters and convert it to lowercase", () => {
-    const output = checkWord("SwЭrd", "Worms");
-    expect(output[2].letter).toEqual("э");
-  });
-
-  it("if the words are not the same, should check the char positions and place the correct answer in result", () => {
-    const output = checkWord("CYCLA", "HALLÅ");
-    expect(output).toEqual([
-      { letter: "c", result: "incorrect" },
-      { letter: "y", result: "incorrect" },
-      { letter: "c", result: "incorrect" },
-      { letter: "l", result: "correct" },
-      { letter: "a", result: "misplaced" },
-    ]);
-  });
-
-  it("check if the inputs are valid strings of the same length by throwing error", () => {
-    expect(() => checkWord(123, "word")).toThrow("Words must be strings of the same length");
-  });
-});
 
 describe("chooseWord()", () => {
   it("should return a string from list", () => {
--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,23 +1,43 @@
 import express from "express";
 import { chooseWord } from "../logic/chooseWord.js";
+import { checkWord } from "../logic/checkWord.js";
 import { wordList } from "../logic/words.js";
 
 const router = express.Router();
+const games = []; //MongoDB
 
 router.get("/start", (req, res) => {
-  const length = parseInt(req.query.length);
-  const unique = req.query.unique;
+
+  const playerId = req.body.playerId; //Req from DB in future
+  const length = parseInt(req.body.length);
+  const unique = req.body.unique === 'true';
+
+  if (!playerId) {
+    return res.status(400).json({ error: "Player ID is required." });
+  }
 
   try {
-    const currentWord = chooseWord(wordList, length, unique);
-    res.json({ word: currentWord, length });
+    games[playerId] = { word: chooseWord(wordList, length, unique)};
+    console.log(games[playerId]);
+    res.json({ status: `Game for playerId ${playerId} is started`, length });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-router.post("/guess", (req, res) => {
 
+router.post("/guess", (req, res) => {
+  const playerId = req.body.playerId;
+  const guess = req.body.guess;
+
+  if (!playerId || !games[playerId]) {
+    return res.status(400).json({ error: "Game is found" });
+  }
+
+  const currentGame = games[playerId];
+  const result = checkWord(guess, currentGame.word, playerId);
+
+  res.json({ result, guess, });
 });
 
 export default router;

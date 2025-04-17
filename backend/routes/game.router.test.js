d5543b9da73d2d6ceff6fb123d117eec7d35087b
Kirill <kirillsavorin@gmail.com>
2025-04-17 22:49:53 +0200

    Jest DP connected Test runs online

--- backend/routes/game.router.test.js
+++ backend/routes/game.router.test.js
@@ -1,12 +1,25 @@
+import { describe, expect, it, jest } from '@jest/globals';
 import request from "supertest";
 import express from "express";
 import gameRouter from "./game.router.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-app.use(express.json()); // Важно для парсинга req.body
+app.use(express.json());
 app.use("/api/games", gameRouter);
 
+
 describe("Api", () => {
+  beforeEach(async() => {
+      await mongoose.connect(process.env.MONGO);
+  });
+
+  afterEach(async() => {
+    await mongoose.disconnect();
+  });
+
   describe("POST /api/games", () => {
     it("should start a new game and return game details", async () => {
       const gameSettings = {
@@ -26,52 +39,4 @@ describe("Api", () => {
       );
     });
   });
-
-  describe("POST /api/games/:id/guesses", () => {
-    let gameId;
-
-    beforeAll(async () => {
-      const gameSettings = {
-        playerId: "GuessTester",
-        length: 5,
-        unique: false,
-        lang: "english",
-      };
-      const startResponse = await request(app)
-        .post("/api/games")
-        .send(gameSettings);
-      gameId = startResponse.body.gameId;
-      expect(gameId).toBeDefined();
-    });
-
-    it("should accept a valid guess and return results", async () => {
-      const guessData = {
-        guess: "hello",
-        highscore: false,
-      };
-
-      const response = await request(app)
-        .post(`/api/games/${gameId}/guesses`)
-        .send(guessData);
-
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty("result");
-      expect(response.body.result.length).toBe(5);
-    });
-
-    it("should return 400 for an invalid guess length", async () => {
-      const guessData = {
-        guess: "hi",
-        highscore: false,
-      };
-
-      const response = await request(app)
-        .post(`/api/games/${gameId}/guesses`)
-        .send(guessData);
-
-      expect(response.statusCode).toBe(400);
-      expect(response.body.msg).toBe("Invalid guess");
-      expect(response.body.status).toContain("should be 5 characters long");
-    });
-  });
-});
+})

3d77cd6a8df06f081330b8ef09abdefc3ab5cf50
Kirill <kirillsavorin@gmail.com>
2025-04-18 08:53:40 +0200

    Jest: POST /api/:id/guesses

--- backend/routes/game.router.test.js
+++ backend/routes/game.router.test.js
@@ -1,42 +1,57 @@
 import { describe, expect, it, jest } from '@jest/globals';
-import request from "supertest";
-import express from "express";
-import gameRouter from "./game.router.js";
-import mongoose from "mongoose";
+import request from 'supertest';
+import express from 'express';
+import gameRouter from './game.router.js';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use("/api/games", gameRouter);
+app.use('/api/games', gameRouter);
 
-
-describe("Api", () => {
-  beforeEach(async() => {
-      await mongoose.connect(process.env.MONGO);
+describe('Api & database', () => {
+  beforeEach(async () => {
+    await mongoose.connect(process.env.MONGO);
   });
 
-  afterEach(async() => {
+  afterEach(async () => {
     await mongoose.disconnect();
   });
 
-  describe("POST /api/games", () => {
-    it("should start a new game and return game details", async () => {
+  describe('POST /api/games', () => {
+    it('should start a new game and return status 200 and object with {status, gameStarted, gameId, language}', async () => {
       const gameSettings = {
-        playerId: "TestPlayer",
+        playerId: 'TestPlayer',
         length: 5,
         unique: false,
-        lang: "swedish",
+        lang: 'swedish',
       };
 
-      const response = await request(app).post("/api/games").send(gameSettings);
+      const response = await request(app).post('/api/games').send(gameSettings);
+      console.log(response.body);
 
       expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty("gameId");
-      expect(response.body.status).toContain(`Player ${gameSettings.playerId}`);
-      expect(response.body.status).toContain(
-        `${gameSettings.length} characters long`
-      );
+      expect(response.body.status).toEqual(expect.any(String));
+      expect(new Date(response.body.gameStarted).getTime()).toBeGreaterThan(0);
+      expect(response.body.gameId).toEqual(expect.any(String));
+      expect(response.body.language).toEqual('swedish');
+      // expect(response.body.status).toContain(`${gameSettings.length} characters long`);
     });
   });
-})
+
+  describe('POST /api/:id/guesses', () => {
+    it('should accept a guess and highscore using the gameId from /api/games', async () => {
+      const responseS = await request(app).post('/api/games').send({playerId:'user', length: 4, unique: false, lang: 'swedish'});
+
+      const guess = {
+        guess: 'word',
+        highscore: false,
+      }
+      const response = await request(app).post(`/api/games/${responseS.body.gameId}/guesses`).send(guess);
+      console.log(responseS.body.gameId);
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+});
